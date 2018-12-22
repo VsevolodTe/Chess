cb116c3c2d55b10f40624e014321a1afc780645b
Гость <Guest@mbp126-2.local>
2018-12-22 10:01:24 +0300

    Initial Commit

---
+++ Chess/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Chess
+//
+//  Created by Гость on 22.12.2018.
+//  Copyright © 2018 Гость. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
